c31ca7084d96f69d32d17c88ba8d018bacc15e47
zerzal <40403983+zerzal@users.noreply.github.com>
2018-06-22 15:33:18 -0400

    Update sendm.pl

--- sendm.pl
+++ sendm.pl
@@ -32,8 +32,6 @@ $smtp->datasend("Subject: LEAVE REQUEST - FOR YOUR RECORDS\n");
 $smtp->datasend($attachment);
 $smtp->datasend("\n"); # done with header
 $smtp->datasend("DOUBLE CLICK ON ATTACHMENT TO ADD EVENT TO OUTLOOK CALENDAR\n\n");
-$smtp->datasend("Content-Type: application/text; name=\"$attachment\"\n");
-$smtp->datasend("Content-Disposition: attachment; filename=\"$attachment\"\n");
 $smtp->dataend();
 $smtp->quit(); # all done. message sent.
 
