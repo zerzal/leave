dc5e23c10bddca52460a1a8ad13717fe6ae33694
zerzal <40403983+zerzal@users.noreply.github.com>
2018-06-25 11:13:51 -0400

    Update sendm.pl

--- sendm.pl
+++ sendm.pl
@@ -1,5 +1,5 @@
 #!/afs/isis/pkg/perl/bin/perl
-use Net::SMTP;
+use Mime::Lite;
 # SEND MAIL SCRIPT 
 
 #$mailprog = 'sendmail';
@@ -21,26 +21,27 @@ $attachment = 'leave.ics'; # un-rem for production
    if ($fields[4]){
    
    #email to requester
-my $smtp = Net::SMTP->new('relay.unc.edu') or die $!;
-$smtp->mail( $super );
-$smtp->to( $fields[4] );
-$smtp->data();
-$smtp->datasend("To: $fields[4]\n");
-$smtp->datasend("From: $super\n");
-$smtp->datasend("Subject: LEAVE REQUEST - FOR YOUR RECORDS\n");
-$smtp->datasend("\n"); # done with header
+	$msg = MIME::Lite->new(
+        From    => $super,
+        To      => $fields[4],
+        Subject => 'LEAVE REQUEST - FOR YOUR RECORDS',
+        Type    =>'multipart/mixed'
+		);
+		
+	$msg->attach(
+        Type     => 'TEXT',
+        Data     => "DOUBLE CLICK ON ATTACHMENT TO ADD EVENT TO OUTLOOK CALENDAR\n\n ENTERED: $fields[0]\n PID: $fields[1]\n NAME: $fields[2]\n BEGIN: $fields[5]\n END: $fields[6]\n TYPE: $fields[7]\n COMMENTS: $fields[8]\n",
+		);
 
-$smtp->datasend("MIME-Version: 1.0\n");
+	$msg->attach(
+        Type        =>'text/plain',
+        Path        => $attachment,
+        Disposition => 'attachment'
+		);
+	MIME::Lite->send('smtp','relay.unc.edu',Debug=>0);	
+	$msg->send();
 
-$smtp->datasend("Content-Disposition: attachment; filename=\$attachment\n");
-
-$smtp->datasend("Content-Type: application/text; name= leave.ics ");
-
-$smtp->datasend();
-
-$smtp->datasend("DOUBLE CLICK ON ATTACHMENT TO ADD EVENT TO OUTLOOK CALENDAR\n\n");
-$smtp->dataend();
-$smtp->quit(); # all done. message sent.
+	# all done. message sent.
 
    #open (MAIL, "|$mailprog $fields[4]") || die "Can't open $mailprog!\n";
    #print MAIL "Reply-to: $super\n";
@@ -64,17 +65,27 @@ $smtp->quit(); # all done. message sent.
    $fields[4] = $nr;
    }
    #email to supervisor
-   my $smtp = Net::SMTP->new('relay.unc.edu') or die $!;
-$smtp->mail( $fields[4] );
-$smtp->to( "dwayne\@n4mio.com" );
-$smtp->data();
-$smtp->datasend("To: dwayne\@n4mio.com\n");
-$smtp->datasend("From: $fields[4]\n");
-$smtp->datasend("Subject: $fields[2] / $fields[5] to $fields[6] / $fields[7]\n\n");
-$smtp->datasend("\n"); # done with header
-$smtp->datasend("DOUBLE CLICK ON ATTACHMENT TO ADD EVENT TO OUTLOOK CALENDAR\n\n");
-$smtp->dataend();
-$smtp->quit(); # all done. message sent.
+   $msg = MIME::Lite->new(
+        From    => $super,
+        To      => $fields[4],
+        Subject => "Subject: $fields[2] / $fields[5] to $fields[6] / $fields[7]\n\n",
+        Type    =>'multipart/mixed'
+		);
+		
+	$msg->attach(
+        Type     => 'TEXT',
+        Data     => "DOUBLE CLICK ON ATTACHMENT TO ADD EVENT TO OUTLOOK CALENDAR\n\n ENTERED: $fields[0]\n PID: $fields[1]\n NAME: $fields[2]\n BEGIN: $fields[5]\n END: $fields[6]\n TYPE: $fields[7]\n COMMENTS: $fields[8]\n",
+		);
+
+	$msg->attach(
+        Type        =>'text/plain',
+        Path        => $attachment,
+        Disposition => 'attachment'
+		);
+	MIME::Lite->send('smtp','relay.unc.edu',Debug=>0);	
+	$msg->send();
+	
+	# all done. message sent.
 
 
    #open (MAIL, "|$mailprog $super") || die "Can't open $mailprog!\n";
